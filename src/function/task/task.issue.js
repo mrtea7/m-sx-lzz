06c56562118d70d80878e839887047755e3de529
wenling-lichao <wenling.lichao@gmail.com>
2015-07-13 10:19:45 +0800

    重新定义样式设计

--- src/function/task/task.issue.js
+++ src/function/task/task.issue.js
@@ -1,32 +1,142 @@
 /*
   目录：
-  task-body
-  save
 */
 
 
-
 /*--------------------------
-  $ task-body
+  $ 切换文字/录音模式
 --------------------------*/
-/*
- 功能：切换为音频输入
- */
+// 功能：切换为音频输入
 $('.js-task-btn-text').tap(function () {
   _toggleTextBody(false);
   _toggleAudioBody(true);
-  // 开启遮罩层
   _toggleMask(true)
 });
 
-/*
- 功能：切换为文本输入
- */
+// 功能：切换为文本输入
 $('.js-task-btn-audio').tap(function () {
   _toggleTextBody(true);
   _toggleAudioBody(false);
   _toggleMask(false)
 });
+
+/*--------------------------
+  $ 录音功能
+--------------------------*/
+var record = $('#record')[0];
+var mask = $('#mask')[0];
+record.addEventListener('touchmove', function (e) {
+  e.preventDefault();
+})
+
+mask.addEventListener('touchmove', function (e) {
+  e.preventDefault();
+})
+
+// Hammer Setup
+var recordGesture = Hammer(record, {});
+var bodyGesture = Hammer(document.documentElement, {});
+//  var bodyH = document.documentElement.clientHeight;
+
+var maskOffsetTop = mask.offsetTop;
+var maskClientHeight = mask.clientHeight;
+var maskBottomSideY = maskOffsetTop + maskClientHeight;
+
+// 定义控制变量
+var isRecordCanel = false;
+var isRecordStart = false;
+
+// 按住录音
+recordGesture.on('press', function (e) {
+  isRecordStart = true;
+  var indicator = $('record-indicator');
+  if (indicator.length == 0) {
+    $('#mask').append('<div id="record-indicator" class="record-indicator">');
+    console.log($('#record-mask').length);
+  } else {
+    indicator.show();
+    indicator.css('backgroundPosition', '0px 0px')
+    //indicator[0].style.backgroundPosition = '0px 0px';
+  }
+  console.log('press');
+})
+
+bodyGesture.on('panup', function (e) {
+  if (isRecordStart) {
+    // 指定下移到特定的区域才更改状态
+    var touchY = e.pointers[0].pageY;
+    if (maskBottomSideY - touchY > maskClientHeight) {
+      _closeRecordMask();
+      isRecordCanel = true;
+    }
+
+    if (maskBottomSideY - touchY >= 100) {
+      $('#record-mask')[0].style.backgroundPosition = '0px -152px';
+      isRecordCanel = true;
+    }
+  }
+}).on('pandown', function (e) {
+  if (isRecordStart) {
+    // 指定下移到特定的区域才更改状态
+    var touchY = e.pointers[0].pageY;
+    if (maskBottomSideY - touchY < 100) {
+      $('#record-mask')[0].style.backgroundPosition = '0px 0px';
+      isRecordCanel = false;
+    }
+  }
+}).on('panend', function (e) {
+  if (isRecordStart) {
+    console.log('isRecordCanel = ', isRecordCanel);
+    _closeRecordMask();
+
+  }
+}).on('pressup', function (e) {
+  console.log('pressup');
+  _closeRecordMask();
+})
+
+function _closeRecordMask(){
+  if (isRecordStart) {
+    $('#record-mask').hide();
+    isRecordStart = false;
+  }
+}
+
+
+/*--------------------------
+  $ 播放录音
+--------------------------*/
+
+
+/*--------------------------
+  $ 重录
+--------------------------*/
+
+
+/*--------------------------
+  $ 停止录音
+--------------------------*/
+
+
+/*--------------------------
+  $ 试听
+--------------------------*/
+
+
+
+
+
+/*--------------------------
+  $ 创建任务
+--------------------------*/
+
+
+
+
+
+/*--------------------------
+  $ 辅助内部函数
+--------------------------*/
 
 function _toggleTextBody(bool){
   if (bool) {
@@ -76,95 +186,7 @@ function _toggleMask(bool){
 
 
 
-$('#issue').on('touchstart', function(){
-  console.log('');
-  //window.location = 'list.html';
-})
 
 
 
-/*--------------------------
-  $ 录音功能
---------------------------*/
-var record = $('#record')[0];
-var mask = $('#mask')[0];
-record.addEventListener('touchmove', function (e) {
-  e.preventDefault();
-})
 
-mask.addEventListener('touchmove', function (e) {
-  e.preventDefault();
-})
-
-/*--------------------------
- $ Hammer Setup
- --------------------------*/
-var recordGesture = Hammer(record, {});
-var bodyGesture = Hammer(document.documentElement, {});
-//  var bodyH = document.documentElement.clientHeight;
-
-var maskOffsetTop = mask.offsetTop;
-var maskClientHeight = mask.clientHeight;
-var maskBottomSideY = maskOffsetTop + maskClientHeight;
-
-/*--------------------------
- $ 录音配置
- --------------------------*/
-
-// 控制变量
-var isRecordCanel = false;
-var isRecordStart = false;
-
-//  按住录音
-recordGesture.on('press', function (e) {
-  isRecordStart = true;
-  if ($('#record-mask').length == 0) {
-    $('#mask').append('<div id="record-mask" class="record-mask">');
-    console.log($('#record-mask').length);
-  } else {
-    $('#record-mask').show();
-    $('#record-mask')[0].style.backgroundPosition = '0px 0px';
-  }
-  console.log('press');
-})
-
-bodyGesture.on('panup', function (e) {
-  if (isRecordStart) {
-    // 指定下移到特定的区域才更改状态
-    var touchY = e.pointers[0].pageY;
-    if (maskBottomSideY - touchY > maskClientHeight) {
-      _closeRecordMask();
-      isRecordCanel = true;
-    }
-
-    if (maskBottomSideY - touchY >= 100) {
-      $('#record-mask')[0].style.backgroundPosition = '0px -152px';
-      isRecordCanel = true;
-    }
-  }
-}).on('pandown', function (e) {
-  if (isRecordStart) {
-    // 指定下移到特定的区域才更改状态
-    var touchY = e.pointers[0].pageY;
-    if (maskBottomSideY - touchY < 100) {
-      $('#record-mask')[0].style.backgroundPosition = '0px 0px';
-      isRecordCanel = false;
-    }
-  }
-}).on('panend', function (e) {
-  if (isRecordStart) {
-    console.log('isRecordCanel = ', isRecordCanel);
-    _closeRecordMask();
-
-  }
-}).on('pressup', function (e) {
-  console.log('pressup');
-  _closeRecordMask();
-})
-
-function _closeRecordMask(){
-  if (isRecordStart) {
-    $('#record-mask').hide();
-    isRecordStart = false;
-  }
-}
